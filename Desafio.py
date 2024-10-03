f44e3d5c2e814bde00ff1d67e5cb71cd72fd7bed
Miranda <pedrohmiranda2106@gmail.com>
2024-10-03 13:22:12 -0300

    Update Desafio.py

--- Desafio.py
+++ Desafio.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 
